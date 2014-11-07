4b3bb3af77dfca299f74cd9e91f33839f2e057fe
nilcy <nilcy@mac.com>
2014-11-07 14:09:17 +0900

    1: 羅針盤を創る
    Task-Url: https://github.com/aynu/mosir/issues/issues/issue/1
    
    Signed-off-by: nilcy <nilcy@mac.com>

---
+++ mosir-core/mosir-core-enterprise/src/main/java/com/github/aynu/mosir/core/testing/package-info.java
@@ -0,0 +1,10 @@
+// ----------------------------------------------------------------------------
+// Copyright (C) Aynu Evolution Laboratory. All rights reserved.
+// GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007
+// http://www.gnu.org/licenses/gpl-3.0-standalone.html
+// ----------------------------------------------------------------------------
+/**
+ * テスティング共通モジュール
+ * @author nilcy
+ */
+package com.github.aynu.mosir.core.testing;
---
+++ mosir-core/mosir-core-standard/src/test/java/com/github/aynu/mosir/core/testing/HelloWorldTest.java
@@ -0,0 +1,20 @@
+// ----------------------------------------------------------------------------
+// Copyright (C) Aynu Evolution Laboratory. All rights reserved.
+// GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007
+// http://www.gnu.org/licenses/gpl-3.0-standalone.html
+// ----------------------------------------------------------------------------
+package com.github.aynu.mosir.core.testing;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+import org.junit.Test;
+/**
+ * {@link HelloWorld} のユニットテスト
+ * @author nilcy
+ */
+@SuppressWarnings({ "static-method", "javadoc" })
+public class HelloWorldTest {
+    @Test
+    public void test() {
+        assertThat(new HelloWorld().say(), is("Hello world!"));
+    }
+}
