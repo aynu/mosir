cd2b311a13753e59a2c8de5ff59b4a6feeea82ed
nilcy <nilcy@mac.com>
2014-11-07 11:20:07 +0900

    Signed-off-by: nilcy <nilcy@mac.com>

---
+++ mosir-core/mosir-core-testing/src/main/java/com/github/aynu/mosir/core/testing/HelloWorld.java
@@ -0,0 +1,20 @@
+// ----------------------------------------------------------------------------
+// Copyright (C) Yukar Evolution Laboratory. All rights reserved.
+// GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007
+// http://www.gnu.org/licenses/gpl-3.0-standalone.html
+// ----------------------------------------------------------------------------
+package com.github.aynu.mosir.core.testing;
+/**
+ * Hello world!
+ * @author nilcy
+ */
+@SuppressWarnings("static-method")
+public class HelloWorld {
+    /**
+     * Say hello
+     * @return "Hello world!"
+     */
+    public String sayHello() {
+        return "Hello world!";
+    }
+}
